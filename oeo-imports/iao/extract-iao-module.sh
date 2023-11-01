85d26760fa268e5f3c2de0f5651a8b82b06ed93d
stap-m <38690039+stap-m@users.noreply.github.com>
2023-11-01 16:05:34 +0100

    Update oeo-imports/iao/extract-iao-module.sh

--- oeo-imports/iao/extract-iao-module.sh
+++ oeo-imports/iao/extract-iao-module.sh
@@ -6,7 +6,7 @@ curl -L https://raw.githubusercontent.com/information-artifact-ontology/IAO/v202
 # Classification:  tbd in oeo-import-edits
 robot merge --input iao-full-download.owl extract --method MIREOT --lower-terms iao-w-hierarchy.txt --intermediates all --upper-term http://purl.obolibrary.org/obo/IAO_0000030 --output iao-extracted.owl
 # Annotates the output module with a commentary to the origin of the content
-robot annotate --input C:/Users/stappel/Documents/Ontologies/test_oeo_imports/iao-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Information Artifact Ontology (IAO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT." --output C:/Users/stappel/Documents/Ontologies/test_oeo_imports/iao-extracted.owl
+robot annotate --input iao-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Information Artifact Ontology (IAO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT." --output iao-extracted.owl
 # Annotates each axiom with the ontology IRI, using prov:wasDerivedFrom
 robot annotate --input C:/Users/stappel/Documents/Ontologies/test_oeo_imports/iao-extracted.owl --annotate-derived-from true --annotate-defined-by true --output C:/Users/stappel/Documents/Ontologies/test_oeo_imports/iao-extracted.owl
 # Annotate with new ontology information
