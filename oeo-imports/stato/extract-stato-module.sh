b6f240b0485946f774112b538dbc223ad327590d
LillyG <lilly.gerlach@ovgu.de>
2024-11-17 23:29:18 +0100

    Change import without hierarchy to import with hierarchy

--- oeo-imports/stato/extract-stato-module.sh
+++ oeo-imports/stato/extract-stato-module.sh
@@ -9,11 +9,11 @@ imports="${ontology_source}/imports"
 # Download the latest STATO release from 2024-07-07
 curl -L https://raw.githubusercontent.com/ISA-tools/stato/refs/heads/dev/releases/1.5/stato.owl > ${tmpdir}/stato-full-download.owl
 # Classification:  tbd in oeo-import-edits
-robot merge --input ${tmpdir}/stato-full-download.owl extract --method MIREOT --lower-terms ${this_wd}/stato-n-hierarchy.txt --upper-term owl:topObjectProperty --intermediates none --output ${tmpdir}/stato-extracted-n-hierarchy.owl
+robot merge --input ${tmpdir}/stato-full-download.owl extract --method MIREOT --lower-terms ${this_wd}/stato-w-hierarchy.txt --upper-term http://purl.obolibrary.org/obo/STATO_0000039 --intermediates all --output ${tmpdir}/stato-extracted-w-hierarchy.owl
 # Create Extracted module and annotate with new ontology information
-robot annotate --input ${tmpdir}/stato-extracted-n-hierarchy.owl --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/stato-extracted.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/stato-extracted.owl --output ${tmpdir}/stato-extracted.owl
+robot annotate --input ${tmpdir}/stato-extracted-w-hierarchy.owl --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/stato-extracted.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/stato-extracted.owl --output ${tmpdir}/stato-extracted.owl
 # Annotates the output module with a commentary to the origin of the content
-robot annotate --input ${tmpdir}/stato-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Statistics Ontology (STATO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the list of selected terms (stato-n-hierarchy.txt) located in the OEO-tools repository." --output ${tmpdir}/stato-extracted.owl
+robot annotate --input ${tmpdir}/stato-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Statistics Ontology (STATO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the list of selected terms (stato-w-hierarchy.txt) located in the OEO-tools repository." --output ${tmpdir}/stato-extracted.owl
 # Annotates each axiom with the ontology IRI, using prov:wasDerivedFrom
 robot annotate --input ${tmpdir}/stato-extracted.owl --annotate-derived-from true --annotate-defined-by true --output ${imports}/stato-extracted.owl
 
