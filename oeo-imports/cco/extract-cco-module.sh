44b31f7dc85c238e0ae220990ecc822fe434b061
ColinHDev <colinheidfeld@gmail.com>
2025-02-13 16:47:26 +0100

    Remove annotation properties during extraction

--- oeo-imports/cco/extract-cco-module.sh
+++ oeo-imports/cco/extract-cco-module.sh
@@ -13,6 +13,8 @@ robot merge --input ${tmpdir}/cco-full-download.ttl extract --method MIREOT --br
 robot merge --input ${tmpdir}/cco-full-download.ttl extract --method MIREOT --lower-terms ${this_wd}/cco-extract-n-hierarchy.txt --intermediates none --output ${tmpdir}/cco-extracted-n-hierarchy.owl
 # Create Extracted module and annotate with new ontology information
 robot merge --input ${tmpdir}/cco-extracted-w-hierarchy.owl --input ${tmpdir}/cco-extracted-n-hierarchy.owl annotate --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/cco-extracted.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/cco-extracted.owl --output ${tmpdir}/cco-extracted.owl
+# Remove definition of known annotation properties from the import
+robot remove --input ${tmpdir}/cco-extracted.owl --select annotation-properties --trim false --output ${tmpdir}/cco-extracted.owl
 # Annotates the output module with a commentary to the origin of the content
 robot annotate --input ${tmpdir}/cco-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Common Core Ontologies (CCO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the list of selected terms (cco-extract-w-hierarchy.txt, cco-extract-n-hierarchy.txt) located in the OEO-tools repository." --output ${tmpdir}/cco-extracted.owl
 # Annotates each axiom with the ontology IRI, using prov:wasDerivedFrom
