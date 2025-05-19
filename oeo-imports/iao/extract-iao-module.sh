75a015c277ed0e709119e6fe307ab7c8e4cff399
Colin <colinheidfeld@gmail.com>
2025-05-19 08:49:40 +0200

    Replaced all occurrences of old iri with new iri

--- oeo-imports/iao/extract-iao-module.sh
+++ oeo-imports/iao/extract-iao-module.sh
@@ -15,13 +15,13 @@ robot merge --input ${tmpdir}/iao-full-download.owl extract --method MIREOT --lo
 # Extract the terms we want without hierarchy
 robot merge --input ${tmpdir}/iao-full-download.owl extract --method MIREOT --lower-terms ${this_wd}/iao-n-hierarchy.txt --upper-term owl:topObjectProperty --intermediates none --output ${tmpdir}/iao-extracted-n-hierarchy.owl
 # Create Extracted module and annotate with new ontology information
-robot merge --input ${tmpdir}/iao-extracted-w-hierarchy.owl --input ${tmpdir}/iao-extracted-n-hierarchy.owl annotate --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/iao-extracted.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/iao-extracted.owl --output ${tmpdir}/iao-extracted.owl
+robot merge --input ${tmpdir}/iao-extracted-w-hierarchy.owl --input ${tmpdir}/iao-extracted-n-hierarchy.owl annotate --ontology-iri https://openenergyplatform.org/ontology/oeo/imports/iao-extracted.owl --version-iri https://openenergyplatform.org/ontology/oeo/dev/imports/iao-extracted.owl --output ${tmpdir}/iao-extracted.owl
 # Annotates the output module with a commentary to the origin of the content
 robot annotate --input ${tmpdir}/iao-extracted.owl --annotation rdfs:comment "This file contains externally imported content from the Information Artifact Ontology (IAO) for import into the Open Energy Ontology (OEO). It is automatically extracted using ROBOT from the list of selected terms (iao-extract-w-hierarchy.txt, iao-extract-n-hierarchy.txt) located in the OEO-tools repository." --output ${tmpdir}/iao-extracted.owl
 # Annotates each axiom with the ontology IRI, using prov:wasDerivedFrom
 robot annotate --input ${tmpdir}/iao-extracted.owl --annotate-derived-from true --annotate-defined-by true --output ${imports}/iao-extracted.owl
 ## Annotate with new ontology information
-#robot annotate --input iao-extracted.owl  --ontology-iri http://openenergy-platform.org/ontology/oeo/imports/iao-extracted.owl --version-iri http://openenergy-platform.org/ontology/oeo/dev/imports/iao-extracted.owl --output iao-extracted.owl
+#robot annotate --input iao-extracted.owl  --ontology-iri https://openenergyplatform.org/ontology/oeo/imports/iao-extracted.owl --version-iri https://openenergyplatform.org/ontology/oeo/dev/imports/iao-extracted.owl --output iao-extracted.owl
 rm ${tmpdir}/iao-full-download.owl
 rm ${tmpdir}/iao-extracted-w-hierarchy.owl
 rm ${tmpdir}/iao-extracted-n-hierarchy.owl
